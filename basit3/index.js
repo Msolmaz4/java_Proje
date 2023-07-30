037479cb551a810f70b085dea38a39b070968341
Your Name <you@example.com>
2023-07-30 23:57:13 +0200

    anlik toplma ve inpu toplma

--- basit3/index.js
+++ basit3/index.js
@@ -2,11 +2,19 @@ const in1 = document.querySelector('.input1')
 const in2 = document.querySelector(".input2")
 const hesap=document.querySelector('.hesapla')
 const sonuc = document.querySelector('.sonuc')
+const ekle= document.querySelector('.ekle')
 
 
 hesap.addEventListener("click",()=>{
     let deg=  Number(in1.value) +Number(in2.value)
     console.log(deg)
     sonuc.innerHTML =   `<p> sonuc :${deg} </p>`
+    const er = document.createElement('div')
+    const ty= document.createElement("p")
+    ty.setAttribute("class","p-yrni")
+    ty.style.background = 'red'
+ ty.textContent= "tebrikler"
+    er.appendChild(ty)
+    ekle.appendChild(er)
 })
 
