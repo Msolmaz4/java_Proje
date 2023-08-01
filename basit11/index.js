96cbe36ef597d09fafa5927251ea30303ed4b0a3
mustafa <msolmaz83@gmail.com>
2023-08-02 00:43:30 +0200

    tel=cif

--- basit11/index.js
+++ basit11/index.js
@@ -3,3 +3,16 @@ console.log("cember daraliyor")
 const der = document.querySelector('.der')
 console.log(der)
 const tkc = prompt("bir sayi giriniz")
+
+if(tkc%2 == 0){
+  const p = document.createElement('p')
+  p.innerText = "sayi cift !!!!!"
+  der.appendChild(p)
+
+}
+else{
+    const p = document.createElement('p')
+    p.setAttribute("class","el")
+    p.innerText="tek sayi !!!!"
+    der.appendChild(p)
+}
