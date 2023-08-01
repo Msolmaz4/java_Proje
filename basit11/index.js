389b12483c06716e6312d51f6265044fcc9265ca
mustafa <msolmaz83@gmail.com>
2023-08-02 00:52:36 +0200

    sayfa-yonlen

--- basit11/index.js
+++ basit11/index.js
@@ -2,6 +2,7 @@ console.log("cember daraliyor")
 
 const der = document.querySelector('.der')
 console.log(der)
+const btn = document.querySelector('.btn')
 const tkc = prompt("bir sayi giriniz")
 
 if(tkc%2 == 0){
@@ -15,4 +16,10 @@ else{
     p.setAttribute("class","el")
     p.innerText="tek sayi !!!!"
     der.appendChild(p)
+}
+// btn.addEventListener('click',()=>{
+//     window.location.href= "https://www.youtube.com"
+// })
+btn.onclick=function(){
+    window.location.href= "https://www.youtube.com"
 }
