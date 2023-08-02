e6eed2dacc537d4a1001ecbef1c5494d982eb4eb
mustafa <msolmaz83@gmail.com>
2023-08-03 00:16:57 +0200

    hamburger

---
+++ basit16/index.js
@@ -0,0 +1,8 @@
+
+console.log('derleme')
+const menu = document.querySelector("#menu")
+const ham = document.querySelector("#ham")
+
+menu.addEventListener('click',()=>{
+    ham.style.left = '0vw'
+})
