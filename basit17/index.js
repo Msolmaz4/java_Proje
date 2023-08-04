42ad3879804ca0a4f1347cbc9ce0d2d57fdbca66
mustafa <msolmaz83@gmail.com>
2023-08-04 14:57:39 +0200

    yiplama

--- basit17/index.js
+++ basit17/index.js
@@ -1,27 +1,30 @@
 const trex = document.querySelector('#trex')
 const kaktus = document.querySelector('#kaktus')
 
-function zipla(){
+function zipla() {
     //console.log('ders')
- 
-    if(kaktus.classList != 'kaktus-animate'){
+    if (kaktus.classList != 'kaktus-animate') {
         kaktus.classList.add('kaktus-animate')
     }
-
-
-
-
-if(trex.classList != 'trex-animate'){
-    trex.classList.add('trex-animate');
-    setTimeout(function(){
-        trex.classList.remove('trex-animate')
-    },500)
-
-
-
+    if (trex.classList != 'trex-animate') {
+        trex.classList.add('trex-animate');
+        setTimeout(function () {
+            trex.classList.remove('trex-animate')
+        }, 500)
+    }
 }
 
+// setInterval Javascript ile belirli aralıklar ile aynı işlemi yapmak için geliştirilmiş bir metotdur. Süre milisaniye olarak belirtilir ve belirtilen süre beklendikten sonra istenen kod çalıştırılır. Sonrasında yine belirlenen süre kadar bekleme yapılır ve sonrasında tekrar kod çalıştırılır. Bu böylece devam eder. Eğer program sonlandırılır veya clearInterval çağrılırsa bu işlem durdurulur.
 
 
-
+const mesajYaz = () => {
+    console.log("setIterval Calisti");
 }
+setInterval(mesajYaz, 5000);
+
+//burada tiklaayinca yiplama ve carpamayi ayarlayacagiy ;nce css cekiyoruy sonra onunda bottom degerini aliyoryuy
+const carpisma = setInterval(function(){
+    var  trexBott =window.getComputedStyle(trex).getPropertyValue('bottom')
+   // console.log(trexBott) sonra bunu parseInt tam ayiya cevirecem
+   
+}) 
