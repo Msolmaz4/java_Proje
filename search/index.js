99f9f9f436937f1dc9e99f505202dab493bceafa
mustafa <msolmaz83@gmail.com>
2023-08-09 12:55:18 +0200

    search_okey

--- search/index.js
+++ search/index.js
@@ -9,7 +9,38 @@ const input = document.querySelector('.input')
 // input.oninput = deger
 const boxs = document.querySelectorAll('.box')
 console.log(boxs)
+//boxdaki degerili kontrol ettim 
 boxs.forEach(function(box1){
-    const name = box1.querySelector('h2').textContent.toLocaleLowerCase()
+    const name = box1.querySelector('h2').textContent.toLocaleLowerCase().trim()
     console.log(name)
 })
+//inputtaki degeri buluyorum
+input.addEventListener('input',()=>{
+    const search = input.value.trim()
+    //console.log(search)
+    filter(search)
+})
+
+const filter =(search)=>{
+
+    const no = document.querySelector('.no')
+    //console.log(no)
+    let num = 0
+    boxs.forEach(function(box){
+        const name = box.querySelector('h2').textContent.toLocaleLowerCase().trim()
+
+        if(name.includes(search.toLocaleLowerCase())){
+            no.style.display='none'
+            box.style.display='flex'
+            num++
+
+        }
+        else{
+            box.style.display='none'
+        }
+    })
+    if(num === 0 ){
+        no.style.display='block'
+    }
+
+}
